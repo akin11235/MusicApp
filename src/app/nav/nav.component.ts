d7505d3bad2afc586e136b5317bab374874079bc
Joseph Oladunmoye <oladunmo@sheridancollege.ca>
2024-05-06 11:21:35 -0400

    InitialCommit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styles: [],
+})
+export class NavComponent {
+  title = 'web304-a2';
+  public searchString: string | undefined;
+
+  constructor(private router: Router) {}
+  handleSearch() {
+    this.router.navigate(['search'], {
+      queryParams: { q: `${this.searchString}`, type: 'artist', limit: '50' },
+    });
+    this.searchString = '';
+  }
+}
